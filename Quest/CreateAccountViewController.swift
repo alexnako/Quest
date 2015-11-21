9f1b506293e21af7c5bda94d92d5105a15291c3b
Haihong Wang <rainbow.wh@gmail.com>
2015-11-21 14:11:22 -0800

    added alerts

--- Quest/CreateAccountViewController.swift
+++ Quest/CreateAccountViewController.swift
@@ -14,13 +14,38 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
+        
+    }
+    
+    func getErrorTitle(error: NSError) -> String {
+        var returnString = ""
+        
+        switch (error.code) {
+        case 200:
+            returnString = "Missing User Name"
+        default:
+            returnString = error.description
+        }
+        return returnString;
+    }
+    
+    
+    func getErrorMessage(error: NSError) -> String {
+        var returnString = ""
+        
+        switch (error.code) {
+        case 200:
+            returnString = "Oops! Please type in your user name"
+        default:
+            returnString = error.description
+        }
+        return returnString;
     }
     
     // CREATE ACCOUNT
@@ -28,16 +53,27 @@ class CreateAccountViewController: UIViewController {
         let user = PFUser()
         user.username = userNameField.text
         user.password = passwordField.text
-        user.signUpInBackgroundWithBlock { (status: Bool, error: NSError?) -> Void in
-            self.performSegueWithIdentifier("createAccountSegue", sender: self)
+        user.email = userNameField.text
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if (succeeded) {
+                // Successfully created account
+                self.performSegueWithIdentifier("createAccountSegue", sender: self)
+            } else {
+                
+                // Parse.com returns error
+                let alertController = UIAlertController(title: self.getErrorTitle(error!), message:
+                    self.getErrorMessage(error!), preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
         }
     }
-
-
+    
     // CANCELLING CREATE ACCOUNT
     @IBAction func didPressCancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-
-
+    
+    
 }
