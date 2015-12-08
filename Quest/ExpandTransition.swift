9eeea99036029994d5b9c4e3dff629cfe9caa2a1
Haihong Wang <rainbow.wh@gmail.com>
2015-12-08 11:35:00 -0800

    edit dismiss transtion

--- Quest/ExpandTransition.swift
+++ Quest/ExpandTransition.swift
@@ -22,8 +22,8 @@ class ExpandTransition: BaseTransition {
         toViewController.view.alpha = 0
         
         UIView.animateWithDuration(0.9, delay: 0.1, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
-                cardHome.selectedCard.transform = CGAffineTransformMakeScale(1, 1)
-                cardHome.selectedCard.frame.origin.y = 0
+            cardHome.selectedCard.transform = CGAffineTransformMakeScale(1, 1)
+            cardHome.selectedCard.frame.origin.y = 0
             }) { (Bool) -> Void in
                 
                 UIView.animateWithDuration(0.5, animations: { () -> Void in
@@ -38,27 +38,25 @@ class ExpandTransition: BaseTransition {
     }
     
     override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
-
+        
         let cardHome = toViewController as! HomeViewController
         
         UIView.animateWithDuration(0.5, animations: { () -> Void in
             fromViewController.view.alpha = 0
             }) { (Bool) -> Void in
                 
-                UIView.animateWithDuration(0.9, delay: 0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
-                        cardHome.selectedCard.transform = CGAffineTransformMakeScale(0.8, 0.8)
-                        cardHome.selectedCard.frame.origin.y = 150
-                    }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(1.6, delay: 0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    cardHome.selectedCard.transform = CGAffineTransformMakeScale(0.8, 0.8)
+                    cardHome.selectedCard.frame.origin.y = 150
+                    cardHome.selectedCard.alpha = 0
+                    }) { (Bool) -> Void in
+                        cardHome.selectedCard.hidden = true
+                        self.finish()
                         
-                        UIView.animateWithDuration(0.2, animations: { () -> Void in
-                            cardHome.selectedCard.alpha = 0
-                            }, completion: { (Bool) -> Void in
-                                cardHome.selectedCard.hidden = true
-                                self.finish()
-                        })
-                })
+                }
         }
         
- 
+        
+        
     }
 }
