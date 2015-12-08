8de443d551196e659a82b795a6eae99cb2b42669
Haihong Wang <rainbow.wh@gmail.com>
2015-12-08 12:55:20 -0800

    changed card dismiss transition.

--- Quest/ExpandTransition.swift
+++ Quest/ExpandTransition.swift
@@ -37,7 +37,7 @@ class ExpandTransition: BaseTransition {
         
     }
     
-    override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
+    /*override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
 
         let cardHome = toViewController as! HomeViewController
         
@@ -60,5 +60,27 @@ class ExpandTransition: BaseTransition {
         }
         
  
+    }*/
+    override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
+        
+        let cardHome = toViewController as! HomeViewController
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            fromViewController.view.alpha = 0
+            }) { (Bool) -> Void in
+                
+                UIView.animateWithDuration(1.2, delay: 0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    cardHome.selectedCard.transform = CGAffineTransformMakeScale(0.8, 0.8)
+                    cardHome.selectedCard.frame.origin.y = 150
+                    cardHome.selectedCard.alpha = 0
+                    }) { (Bool) -> Void in
+                        cardHome.selectedCard.hidden = true
+                        self.finish()
+                        
+                }
+        }
+        
+        
+        
     }
 }
